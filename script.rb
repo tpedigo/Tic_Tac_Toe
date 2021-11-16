37c17f12803b4799bcabfd41df8634e510857983
Tara Pedigo <tkirkhus@gmail.com>
2021-11-16 09:59:08 -0500

    Add gameplay method and check_board method

--- script.rb
+++ script.rb
@@ -46,32 +46,37 @@ class Gameboard < GameSquare
     end
 
     def check_board
-        if @gameboard[0] != " "
-            if (@gameboard[0].play == @gameboard[1].play && @gameboard[1].play == @gameboard[2].play) || (@gameboard[0].play == @gameboard[3].play && @gameboard[3].play == @gameboard[6].play) || (@gameboard[0].play == @gameboard[4].play && @gameboard[4].play == @gameboard[8].play)
-                puts "Winner! #{@gameboard[0].play} got 3 in a row."
-            end
-        elsif @gameboard[1] != " "
-            if (@gameboard[1].play == @gameboard[4].play && @gameboard[4].play == @gameboard[7].play)
-                puts "Winner! #{@gameboard[4].play} got 3 in a row."
-            end
-        elsif @gameboard[2] != " "
-            if (@gameboard[2].play == @gameboard[5].play && @gameboard[5].play == @gameboard[8].play) (@gameboard[2].play == @gameboard[4].play && @gameboard[4].play == @gameboard[6].play)
-                puts "Winner! #{@gameboard[2].play} got 3 in a row."
-            end
-        elsif @gameboard[3] != " "
-            if (@gameboard[3].play == @gameboard[4].play && @gameboard[4].play == @gameboard[5].play)
-                puts "Winner! #{@gameboard[3].play} got 3 in a row."
-            end
-        elsif @gameboard[6] != " "
-            if (@gameboard[6].play == @gameboard[7].play && @gameboard[7].play == @gameboard[8].play)
-                puts "Winner! #{@gameboard[6].play} got 3 in a row."
-            end
+        if (@gameboard[0].play != "0") && ((@gameboard[0].play == @gameboard[1].play && @gameboard[1].play == @gameboard[2].play) || (@gameboard[0].play == @gameboard[3].play && @gameboard[3].play == @gameboard[6].play) || (@gameboard[0].play == @gameboard[4].play && @gameboard[4].play == @gameboard[8].play))
+            puts "Winner! #{@gameboard[0].play} got 3 in a row."
+            return false
+        elsif (@gameboard[1].play != "1") && ((@gameboard[1].play == @gameboard[4].play && @gameboard[4].play == @gameboard[7].play))
+            puts "Winner! #{@gameboard[4].play} got 3 in a row."
+            return false
+        elsif (@gameboard[2].play != "2") && ((@gameboard[2].play == @gameboard[5].play && @gameboard[5].play == @gameboard[8].play) || (@gameboard[2].play == @gameboard[4].play && @gameboard[4].play == @gameboard[6].play))
+            puts "Winner! #{@gameboard[2].play} got 3 in a row."
+            return false
+        elsif (@gameboard[3].play != "3") && ((@gameboard[3].play == @gameboard[4].play && @gameboard[4].play == @gameboard[5].play))
+            puts "Winner! #{@gameboard[3].play} got 3 in a row."
+            return false
+        elsif (@gameboard[6].play != "6") && ((@gameboard[6].play == @gameboard[7].play && @gameboard[7].play == @gameboard[8].play))
+            puts "Winner! #{@gameboard[6].play} got 3 in a row."
+            return false
+        else
+            puts "Next round!"
+            return true
+        end
+    end
+
+    def gameplay
+        playing = true
+        while playing
+            self.visual
+            update_board
+            self.visual
+            playing = check_board
         end
     end
 end
 
 game_board = Gameboard.new()
-
-game_board.visual
-game_board.update_board
-game_board.visual
+game_board.gameplay
